de92ee276f393f238619d279f53eedff7dfe3866
mikolaj <dinuirar@gmail.com>
2020-05-07 16:52:18 +0200

    key vault access attempts

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src import app
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 
 def main():
@@ -6,4 +8,20 @@ def main():
 
 
 if __name__ == '__main__':
+    credential = DefaultAzureCredential()
+
+    # secret_client = SecretClient(vault_url="https://dr-dev-euw-kv-common01.vault.azure.net/",
+    #                             credential=credential)
+    secret_client = SecretClient(vault_url="https://dr-prod-euw-kv01.vault.azure.net/",
+                                credential=credential)
+
+    secret_properties = secret_client.list_properties_of_secrets()
+    for secret_property in secret_properties:
+        print(secret_property.name)
+
+    # secret = secret_client.get_secret("Notifications-SqlDb-ConnectionString")
+    # secret = secret_client.get_secret("Recommendations-Database-ConnectionString")
+    # print(secret.name)
+    # print(secret.value)
+
     main()
